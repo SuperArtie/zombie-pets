dadccb0e46a1e55d4b2b501095d64a119ae4a188
Nathan Vasquez <nathan.superartie@gmail.com>
2014-09-09 16:03:48 -0500

    second commit

--- client/index.js
+++ client/index.js
@@ -1,17 +1,57 @@
 (function(){
   'use strict';
 
-  angular.module('mean-template', [])
-  .controller('MainController', ['$scope', '$interval', function($scope, $interval){
-    var occupations = ['Superheroes', 'Ninjas', 'Pirates', 'Vampires', 'Aliens', 'Dragons', 'Sharks with Lasers', 'Transformers', 'I am Groot'];
-
-    $scope.title = 'Mean Template';
-    $scope.occupation = occupations[0];
-
-    $interval(function(){
-      var rnd = Math.floor(Math.random() * occupations.length);
-      $scope.occupation = occupations[rnd];
-    }, 500);
+  angular.module('zombie-pets', [])
+  .controller('MainController', ['$scope', function($scope){
+    $scope.title = 'Zombie petZ';
+    $scope.toggleWeapon = function(){
+      $scope.hideWeapon = !!!$scope.hideWeapon;
+    };
+    $scope.togglePet = function(){
+      $scope.hidePet = !!!$scope.hidePet;
+    };
+    $scope.weapon = {};
+    $scope.weapons = [];
+    $scope.addWeapon = function(){
+      $scope.weapons.push($scope.weapon);
+      $scope.weapon = {};
+      $('#name').focus();
+    };
+    $scope.pet  = {health:100};
+    $scope.pets = [];
+    $scope.addPet = function(){
+      var index = $scope.pet.weapon * 1;
+      $scope.pet.weapon = $scope.weapons[index];
+      $scope.pets.push($scope.pet);
+      $scope.pet = {health:100};
+      $('#petName').focus();
+    };
+    $scope.player1 = null;
+    $scope.player2 = null;
+    $scope.setPlayer = function(num){
+      $scope['player' + num] = this.p;
+    };
+    $scope.fight = function(){
+      var turn = Math.floor((Math.random()*2) + 1),
+          a    = $scope.player1,
+          b    = $scope.player2;
+      switch(turn){
+        case 1:
+          $scope.attack(a, b);
+          $scope.attack(b, a);
+          break;
+        case 2:
+          $scope.attack(b, a);
+          $scope.attack(a, b);
+      }
+    };
+    $scope.attack = function(player1, player2){
+      if(player1.health > 0){
+        player2.health -= Math.floor((Math.random()*player1.weapon.damage) + 1);
+      }else{
+        player2.health -= Math.floor((Math.random()*3) + 1);
+      }
+    };
   }]);
 })();
 
